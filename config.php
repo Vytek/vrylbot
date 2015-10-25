a4769a9ef0592681660c31fc13ef57096bccebc4
Vytek <enrico.speranza@gmail.com>
2015-10-25 06:58:19 -0400

    Add dummy config.php

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * CONFIG file DO NOT SHARE on GitHub
+ * Replace with your kaeys
+ */
+
+// TELEGRAM BOT ID 
+define('BOT_ID' , '');
+//$bot_id = "";
+
+//API_KEY for Servizi Mobilità
+define('API_KEY', '');
+//$API_KEY = "";
+
+//Twitter Keys
+define('OAUTH_ACCESS_TOKEN', '');
+define('OAUTH_ACCESS_TOKEN_SECRET', '');
+define('CONSUMER_KEY', '');
+define('CONSUMER_SECRET', '');
+?>
